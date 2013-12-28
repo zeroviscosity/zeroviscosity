3abd95e7ad0dc7d645cd6ca3508294d9297aefdf
Kent English <kent.english@gmail.com>
2013-12-28 13:30:12 -0500

    Added 404 and 500 pages

--- app.js
+++ app.js
@@ -7,14 +7,41 @@ var express = require('express'),
 /*
  * App set up
  */
-
 app.configure(function(){
     app.use(express.bodyParser());
-    app.use(app.router);
     app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.locals.pretty = true;
+
+});
+
+/*
+ * Error Handling
+ */
+app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.render('500', { error: err });
 });
 
 /*
