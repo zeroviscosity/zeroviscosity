11804166afbbcc7e88f2d695cc536832990ad938
Kent English <kent.english@gmail.com>
2014-08-31 16:29:20 -0400

    Switching around app.locals.pretty

--- app.js
+++ app.js
@@ -5,21 +5,19 @@ var express = require('express'),
     port = process.env.NODE_PORT || 3000,
     env = process.env.NODE_ENV || 'development';
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
-app.use(express.static(__dirname + '/public'));
-app.use(goblet(__dirname + '/goblet.json'));
-
 app.enable('verbose errors');
-
-app.locals.pretty = true;
 
 if (env === 'development') {
     app.use(logger('dev'));
 } else if (env === 'production') {
     app.disable('verbose errors');
 }
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
+app.use(goblet(__dirname + '/goblet.json'));
 
 app.listen(port, function() {
     console.log('Listening on port ' + port);
