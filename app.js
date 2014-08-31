afbb3912ecb8ee9162b5f8a2f15699cb6368f199
Kent English <kent.english@gmail.com>
2014-08-31 15:38:56 -0400

    Moved logging, etc. from goblet index into app

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require('express'),
+    logger = require('morgan'),
     goblet = require('./lib/goblet'),
     app = express(),
-    port = process.env.NODE_PORT || 3000;
+    port = process.env.NODE_PORT || 3000,
+    env = process.env.NODE_ENV || 'development';
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 app.use(goblet(__dirname + '/goblet.json'));
+
+app.enable('verbose errors');
+
+app.locals.pretty = true;
+
+if (env === 'development') {
+    app.use(logger('dev'));
+} else if (env === 'production') {
+    app.disable('verbose errors');
+}
 
 app.listen(port, function() {
     console.log('Listening on port ' + port);
