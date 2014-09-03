f9f7aa3dc86dbe4fcdd69ccb70c02c1d54b17267
Kent English <kent.english@gmail.com>
2014-09-03 10:51:23 -0400

    Revised gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
     sass = require('gulp-sass'),
     nodemon = require('gulp-nodemon'),
     jshint = require('gulp-jshint'),
     livereload = require('gulp-livereload'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
     paths = {
-        js: ['./src/js/**/*.js'],
+        js: {
+            lib: ['./bower_components/fastclick/lib/fastclick.js'], 
+            app: ['./src/js/**/*.js']
+        },
         node: ['./*.js', './lib/**/*.js'],
-        sass: ['./src/scss/*.scss'],
+        sass: './src/scss/*.scss',
         dest: {
             css: './public/css',
             js: './public/js'
@@ -15,20 +21,33 @@ var gulp = require('gulp'),
     };
 
 gulp.task('lint', function () {
-    gulp.src(paths.js.concat(paths.node))
+    gulp.src(paths.js.app.concat(paths.node))
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('js', function() {
-    gulp.src(paths.js)
-        .pipe(concat('app.js', { newLine: ';\n' }))
+    gulp.src(paths.js.lib.concat(paths.js.app))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(paths.dest.js));
+});
+
+gulp.task('js:prod', function() {
+    gulp.src(paths.js.lib.concat(paths.js.app))
+        .pipe(uglify())
+        .pipe(concat('app.js'))
         .pipe(gulp.dest(paths.dest.js));
 });
 
 gulp.task('sass', function () {
     gulp.src(paths.sass)
         .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest(paths.dest.css));
+});
+
+gulp.task('sass:prod', function () {
+    gulp.src(paths.sass)
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest(paths.dest.css));
 });
 
@@ -49,4 +68,4 @@ gulp.task('develop', function () {
 });
 
 gulp.task('default', ['lint', 'js', 'sass', 'develop']);
-gulp.task('prod', ['js', 'sass']);
+gulp.task('prod', ['js:prod', 'sass:prod']);
